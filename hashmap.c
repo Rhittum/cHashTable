4df799efb25e28decfa9b1b5d5bf15136625187b
Rhittum <rhittum1802@gmail.com>
2026-07-12 00:29:30 +0530

    SipHash and Murmur mix implemented

--- hashmap.c
+++ hashmap.c
@@ -1,9 +1,27 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 #define HASHMAP_DEFAULT_CAPACITY 32
+
+#define ROTL(x, b) (uint64_t)(((x) << (b)) | ((x) >> (64 - (b))))
+#define SIPROUND                    \
+	do {                        \
+		v0 += v1; v2 += v3; \
+		v1 = ROTL(v1, 13);  \
+		v3 = ROTL(v3, 16);  \
+		v1 ^= v0; v3 ^= v2; \
+		v0 = ROTL(v0, 32);  \
+		v2 += v1; v0 += v3; \
+		v1 = ROTL(v1, 17);  \
+		v3 = ROTL(v3, 21);  \
+		v1 ^= v2; v3 ^= v0; \
+		v2 = ROTL(v2, 32);  \
+	} while (0)
 
 typedef enum {
 	OCCUPIED,
@@ -25,6 +43,15 @@ typedef struct {
 	uint64_t secretKey[2];
 } HashMap;
 
+uint64_t fmix64(uint64_t x) {
+    x ^= x >> 33;
+    x *= 0xff51afd7ed558ccd;
+    x ^= x >> 33;
+    x *= 0xc4ceb9fe1a85ec53;
+    x ^= x >> 33;
+    return x;
+}
+
 HashEntry setEntry(char* key, void* value, SlotState state) {
 	return (HashEntry){
 		.key = key,
@@ -33,7 +60,57 @@ HashEntry setEntry(char* key, void* value, SlotState state) {
 	};
 }
 
-unsigned int sipHash(const char* key, int capacity) {
+uint64_t siphash13(const uint8_t *key, size_t len, uint64_t k0, uint64_t k1) {
+
+	uint64_t v0 = 0x736f6d6570736575ULL ^ k0;
+	uint64_t v1 = 0x646f6e646d61636bULL ^ k1;
+	uint64_t v2 = 0x6c7967656e657261ULL ^ k0;
+	uint64_t v3 = 0x7465646279746573ULL ^ k1;
+
+	const uint8_t *end = key + (len & ~7);
+	const uint8_t *p;
+
+	for (p=key; p<end; p+=8) {
+		uint64_t m;
+		memcpy(&m, p, 8);
+
+		v3^=m;
+		SIPROUND;
+		v0^=m;
+	}
+
+	uint64_t last_block = (uint64_t)(len && 0xff) << 56;
+	switch (len & 7) {
+		case 7: last_block |= (uint64_t)p[6] << 48;
+		case 6: last_block |= (uint64_t)p[5] << 40;
+		case 5: last_block |= (uint64_t)p[4] << 32;
+		case 4: last_block |= (uint64_t)p[3] << 24;
+		case 3: last_block |= (uint64_t)p[2] << 16;
+		case 2: last_block |= (uint64_t)p[1] << 8;
+		case 1: last_block |= (uint64_t)p[0];
+		case 0: break;
+	}
+
+	v3 ^= last_block;
+	SIPROUND;
+	v0 ^= last_block;
+
+	v2 ^= 0xff;
+	SIPROUND;
+	SIPROUND;
+	SIPROUND;
+
+	return v0 ^ v1 ^ v2 ^ v3;
+}
+
+uint32_t getBucketIndex(HashMap *map, const char *key) {
+	size_t len = strlen(key);
+
+	uint64_t hash = siphash13((const uint8_t*)key, len, map->secretKey[0], map->secretKey[1]);
+
+	fmix64(hash);
+
+	return (uint32_t)(hash & (map->capacity - 1));
 }
 
 HashMap* createTable(int capacity) {
